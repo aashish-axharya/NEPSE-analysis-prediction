0510d339c2b7ff5482aae8a747ef0ebd4e04af6d
Aashish <aashish.acharya006@gmail.com>
2022-12-29 14:28:49 +0545

    New Data

--- scrape/convert.py
+++ scrape/convert.py
@@ -2,23 +2,22 @@
 
 import os
 import pandas as pd
-import numpy as np
-import csv
 
 # Load the stock data
-path = 'scrape/data-old'
+path = 'scrape/data'
 files = os.listdir(path)
 df = pd.DataFrame()
 
 for file in files:
     df = pd.read_csv(os.path.join(path, file))
-    new_df = pd.DataFrame()
+    test_df = pd.DataFrame(columns=['SN', 'Symbol','Name','Conf', 'Open', 'High', 'Low', 'Close', 'VWAP', 'Vol', 'Prev Close', 'Turnover', 'Trans', 'Diff', 'Range', 'Diff Percent', 'Range Percent', 'VWAP%', '120 days','180 days','52 weeks high','52 weeks low'])
+
     for i in range(len(df)):
         symbol = df['Symbol'][i]
         test_df = df.loc[df['Symbol'] == symbol]
         test_df = test_df.reset_index(drop=True)
         test_df['Date'] = file.split('.')[0]
 
-        with open('scrape/data/individual/' + symbol + '.csv', 'a+') as f:
+        with open('scrape/test/test/' + symbol + '.csv', 'a+') as f:
             test_df.to_csv(f, header=False, index=False)
 
--- test.py
+++ test.py
@@ -6,15 +6,21 @@ from keras.layers import Dense, LSTM
 import os
 
 # Load the stock data
-path = 'scrape/data-old'
-files = os.listdir(path)
-df = pd.DataFrame()
-for file in files:
-    df = df.append(pd.read_csv(os.path.join(path, file)))
+# path = 'scrape/data/'
+# files = os.listdir(path)
+# df = pd.DataFrame()
+# for file in files:
+#     df = df.append(pd.read_csv(os.path.join(path, file)).dropna())
+#     df.reset_index(drop=True)
+
+df = pd.read_csv('scrape/test/individual/ADBL.csv').dropna()
 
 # Extract the closing price and convert it to a numpy array
 close_prices = df['Close'].values
-close_prices = np.vectorize(lambda x: float(x.replace(',', '')))(close_prices)
+try:
+    close_prices = np.vectorize(lambda x: float(x.replace(',', '')))(close_prices)
+except:
+    pass
 close_prices = close_prices.reshape(-1, 1)
 
 # Scale the data to the range (0, 1)
@@ -22,7 +28,7 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 close_prices = scaler.fit_transform(close_prices)
 
 # Split the data into training and test sets
-split_ratio = 0.8
+split_ratio = 0.7
 train_size = int(len(close_prices) * split_ratio)
 test_size = len(close_prices) - train_size
 train_data, test_data = close_prices[0:train_size,:], close_prices[train_size:len(close_prices),:]
@@ -36,11 +42,11 @@ def create_dataset(data, time_steps=1):
         y.append(data[i + time_steps, 0])
     return np.array(X), np.array(y)
 
-time_steps = 1000
+time_steps = 4
 X_train, y_train = create_dataset(train_data, time_steps)
 X_test, y_test = create_dataset(test_data, time_steps)
 
-# Reshape the data for the LSTM model
+#Reshape the data for the LSTM model
 X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
 X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
@@ -49,7 +55,7 @@ model = Sequential()
 model.add(LSTM(50, return_sequences=True, input_shape=(1, time_steps)))
 model.add(LSTM(50))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='mean_squared_error', optimizer='adam',metrics=['accuracy'])
 
 # Train the model
 model.fit(X_train, y_train, epochs=20, batch_size=32, verbose=2)
@@ -57,8 +63,9 @@ model.fit(X_train, y_train, epochs=20, batch_size=32, verbose=2)
 # Test the model
 predictions = model.predict(X_test)
 predictions = scaler.inverse_transform(predictions)
-print(predictions)
 
+accuracy = model.evaluate(X_test, y_test)
+print(accuracy)
 # Calculate the root mean squared error
 rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
 print(model.summary())
